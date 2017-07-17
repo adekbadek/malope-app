70637e88060f8717688960b0c735fcd72915d071
Adam Borowski <adam@adamboro.com>
2017-07-18 00:31:54 +0200

    ImageThumb - read/write custom data as JS object

--- app/components/ImageThumb.jsx
+++ app/components/ImageThumb.jsx
@@ -6,14 +6,16 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 
 import styles from './Home.sass'
 import { EXIF_TAG_NAME, writeComment, normalizePath, mapObjectToPairs } from '../utils/helpers'
+import ObjectBuilder from './ObjectBuilder'
+import Pair from './Pair'
 
 export default class ImageThumb extends React.Component {
-  submitComment = e => {
-    e.preventDefault()
-    writeComment(this.props.image, this.refs.commentTextArea.value)
+  submitCustomData = (object: {}) => {
+    writeComment(this.props.image, JSON.stringify(object))
       .then(this.props.updateCallback)
   }
   render () {
+    const customData = prop(EXIF_TAG_NAME, this.props.image.metadata)
     return (
       <div className={cx('mt4', styles.imageThumb)}>
         <div className='center bt bb pv1'>{this.props.image.name}</div>
@@ -32,20 +34,17 @@ export default class ImageThumb extends React.Component {
               </TabList>
               <TabPanel>
                 <div className='mt3'>
-                  <div>{prop(EXIF_TAG_NAME, this.props.image.metadata)}</div>
-                  <form onSubmit={this.submitComment}>
-                    <textarea ref='commentTextArea' className='w-100 mt2' rows='5' />
-                    <input type='submit' value='Submit' />
-                  </form>
+                  <pre className='mb3'>{customData}</pre>
+                  <ObjectBuilder
+                    onSubmit={this.submitCustomData}
+                    defaultObject={JSON.parse(customData)}
+                  />
                 </div>
               </TabPanel>
               <TabPanel>
                 <div className='mt2'>
                   {mapObjectToPairs(omit([EXIF_TAG_NAME], this.props.image.metadata)).map(v => (
-                    <div className='flex mt1' key={v.key}>
-                      <div className='w-50 dib'>{v.key}</div>
-                      <div className='w-50 dib'>{v.val}</div>
-                    </div>
+                    <Pair key={v.key} pair={v} />
                   ))}
                 </div>
               </TabPanel>
