633571b9746faa79f40f0cfef77508318452669d
Adam Borowski <adam@adamboro.com>
2017-07-17 20:46:05 +0200

    display EXIF data and images

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,15 +1,50 @@
 // @flow
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { pick, assoc } from 'ramda'
+
+import { hashCode, readImageMetadata } from '../utils/helpers'
 import styles from './Home.sass'
 
+const ImageThumb = props =>
+  <div className={styles.imageThumb}>
+    <div className={styles.imageThumbName}>{props.image.name}</div>
+    <pre>{JSON.stringify(props.image.metadata)}</pre>
+    <div
+      className={styles.imageThumbTile}
+      style={{backgroundImage: `url(${props.image.path})`}}
+    />
+  </div>
+
 export default class Home extends Component {
+  state = {
+    images: [],
+  }
+  submitFile = (e: SyntheticEvent) => {
+    const fileList = e.currentTarget.files
+    const images = [].slice.call(fileList).map(file => pick(['path', 'name'], file))
+    Promise.all(
+      images.map(image => readImageMetadata(image.path))
+    )
+      .then(metadata => {
+        this.setState({images: images.map((image, i) => assoc('metadata', metadata[i], image))})
+      })
+  }
   render () {
     return (
       <div className='ph3 ph5-ns'>
         <div className={styles.container} data-tid='container'>
           <h2>Image Tagger</h2>
-          <Link to='/counter'>to Counter</Link>
+          <form>
+            <input multiple type='file' onChange={this.submitFile} />
+          </form>
+          <div className='mt3'>
+            {this.state.images.map(image => (
+              <ImageThumb
+                key={hashCode(image.path)}
+                image={image}
+              />
+            ))}
+          </div>
         </div>
       </div>
     )
--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,3 +1,4 @@
+import exiftool from 'node-exiftool'
 import { compose, map, values } from 'ramda'
 
 export const mapObject = compose(values, map)
@@ -5,3 +6,19 @@ export const mapObject = compose(values, map)
 export const hashCode = str => {
   return str.split('').reduce((prevHash, currVal) => ((prevHash << 5) - prevHash) + currVal.charCodeAt(0), 0)
 }
+
+const EXIFToolProcess = new exiftool.ExiftoolProcess()
+export const readImageMetadata = imagePath => new Promise((resolve, reject) => {
+  const onSuccess = result => {
+    resolve(result.data[0])
+  }
+  if (EXIFToolProcess.isOpen) {
+    EXIFToolProcess.readMetadata(imagePath, ['-File:all'])
+      .then(onSuccess, reject)
+  } else {
+    EXIFToolProcess
+      .open()
+      .then(() => EXIFToolProcess.readMetadata(imagePath, ['-File:all']))
+      .then(onSuccess, reject)
+  }
+})
