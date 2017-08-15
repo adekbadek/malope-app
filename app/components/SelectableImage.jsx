b3869adf260f99a9d73f87f8c3b828a383c3a6d3
Adam Borowski <adam@adamboro.com>
2017-08-15 14:13:02 +0200

    SelectableImage refactor

--- app/components/SelectableImage.jsx
+++ app/components/SelectableImage.jsx
@@ -6,48 +6,33 @@ import { createSelectable } from 'react-selectable-fast'
 import { EXIF_TAG_NAME, normalizePath, jsonParse } from '../utils/helpers'
 import styles from './Home.sass'
 
-class SelectableImage extends React.Component {
-  state = {
-    hasCustomData: false,
-  }
-  componentDidMount () {
-    this.updateCustomData(this.props)
-  }
-  componentWillReceiveProps (nextProps: any) {
-    if (this.props.image.metadata !== nextProps.image.metadata) {
-      this.updateCustomData(nextProps)
-    }
-  }
-  updateCustomData = (props: any) => {
-    const rawCustomData = props.image.metadata[EXIF_TAG_NAME]
-    const customData = rawCustomData && jsonParse(rawCustomData)
-    customData && this.setState({hasCustomData: !isEmpty(customData)})
-  }
-  render () {
-    const { selectableRef, selected, selecting, image } = this.props
-    return (
+const SelectableImage = props => {
+  const rawCustomData = props.image.metadata[EXIF_TAG_NAME]
+  const customData = rawCustomData && jsonParse(rawCustomData)
+  const { selectableRef, selected, selecting, image } = props
+
+  return (
+    <div
+      ref={selectableRef}
+      className={cx('flex--inline pt-card', styles.imageTile, {
+        [styles.imageTileSelected]: selected,
+        [styles.imageTileSelecting]: selecting,
+      })}
+    >
       <div
-        ref={selectableRef}
-        className={cx('flex--inline pt-card', styles.imageTile, {
-          [styles.imageTileSelected]: selected,
-          [styles.imageTileSelecting]: selecting,
-        })}
-      >
-        <div
-          className={styles.imageTileDisplay}
-          style={{backgroundImage: `url(${normalizePath(image.path)})`}}
-        />
-        <div className={styles.imageTileInfo}>
-          <div className={styles.imageTileInfoTitle}>
-            {image.name}
-          </div>
-          <div>
-            {this.state.hasCustomData && <span className='pt-ui-text pt-icon-info-sign' />}
-          </div>
+        className={styles.imageTileDisplay}
+        style={{backgroundImage: `url(${normalizePath(image.path)})`}}
+      />
+      <div className={styles.imageTileInfo}>
+        <div className={styles.imageTileInfoTitle}>
+          {image.name}
+        </div>
+        <div>
+          {customData && customData.tags && !isEmpty(customData.tags) && <span className='pt-ui-text pt-icon-tag' />}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default createSelectable(SelectableImage)
