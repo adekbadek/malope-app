7ebcda92f64fc4c331724fdc4bdb6e652b081de3
Adam Borowski <adam@adamboro.com>
2017-08-28 23:29:52 +0200

    fix CSV parse quote

--- app/utils/csv.js
+++ app/utils/csv.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 
 export const parseCSVFile = (filePath: string, delimiter: string) => new Promise((resolve, reject) => {
   const fileContents = fs.readFileSync(filePath, 'utf8')
-  parse(fileContents, {columns: true, relax_column_count: true, auto_parse: true, delimiter}, (err, table) => {
+  parse(fileContents, {columns: true, quote: false, relax_column_count: true, auto_parse: true, delimiter}, (err, table) => {
     err ? reject(err) : resolve(table)
   })
 })
