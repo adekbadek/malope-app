99a3ef8d816d1fc92df6bec89d892aaece9775d1
Adam Borowski <adam@adamboro.com>
2017-09-10 10:54:23 +0200

    ImageThumb - make stateful, overflow scroll

--- app/components/ImageThumb.jsx
+++ app/components/ImageThumb.jsx
@@ -10,55 +10,73 @@ import {
 import TagsEditor from './TagsEditor'
 import CustomFieldsEditor from './CustomFieldsEditor'
 
-export default ({files, updateCallback, itemsLen, ...props}: any) => {
-  const submitCustomData = (changes?: {}, fileNames = null) => {
-    const filteredFiles = files.filter(file => fileNames ? fileNames.includes(file.name) : file)
+export default class ImageThumb extends React.Component {
+  state = {
+    topOffset: 140,
+  }
+  componentDidMount () {
+    const box = this.refs.main.getBoundingClientRect()
+    this.setState({
+      topOffset: box.top,
+    })
+  }
+  submitCustomData = (changes?: {}, fileNames: any = null) => {
+    const filteredFiles = this.props.files.filter(file => fileNames ? fileNames.includes(file.name) : file)
     Promise.all(filteredFiles.map(updateSingleFile(changes)))
-      .then(() => updateCallback(filteredFiles, true))
+      .then(() => this.props.updateCallback(filteredFiles, true))
   }
 
-  const getRawCustomDataForFiles = () => files.map(v => v.data)
-  const getRawDataForFiles = () => files.map(v => v.metadata)
+  getRawCustomDataForFiles = () => this.props.files.map(v => v.data)
+  getRawDataForFiles = () => this.props.files.map(v => v.metadata)
 
-  const removeAllData = () => submitCustomData()
+  removeAllData = () => this.submitCustomData()
 
-  const getPanel = (data) => (
+  getPanel = (data: any) => (
     sameValues(data)
       ? <pre className='mb-10'>{JSON.stringify(data[0])}</pre>
       : <div className='mtb-10 pt-callout pt-intent-warning'>
           Data differs between selected files.
       </div>
   )
-
-  return (
-    <div className='mt-20'>
-      <div>{
-        `Editing ${pluralize('item', itemsLen)}: ${files.map(v => v.name).join(', ')}`
-      }</div>
-      <div>
-        <div className='mt-20'>
-          <Tabs2 className='custom-tabs-panel' animate={false}>
-            <Tab2 id='custom' title='Custom Data' panel={getPanel(getRawCustomDataForFiles())} />
-            <Tab2 id='all' title='All Metadata' panel={getPanel(getRawDataForFiles())} />
-          </Tabs2>
-          <TagsEditor
-            files={files}
-            submitHandler={submitCustomData}
-            allTags={props.allTags}
-          />
-          <CustomFieldsEditor
-            files={files}
-            allCustomFieldsKeys={props.allCustomFieldsKeys}
-            submitHandler={submitCustomData}
-          />
-          <div className='center'>
-            <Button
-              className='pt-intent-danger mt-30'
-              onClick={() => window.confirm('Are you sure?') && removeAllData()}
-            >remove all data</Button>
+  render () {
+    const {files, itemsLen, ...props} = this.props
+    return (
+      <div
+        className='mt-20 pb-20'
+        ref='main'
+        style={{
+          overflow: 'scroll',
+          maxHeight: `calc(100vh - ${this.state.topOffset}px)`
+        }}
+      >
+        <div>{
+          `Editing ${pluralize('item', itemsLen)}: ${files.map(v => v.name).join(', ')}`
+        }</div>
+        <div>
+          <div className='mt-20'>
+            <Tabs2 className='custom-tabs-panel' animate={false}>
+              <Tab2 id='custom' title='Custom Data' panel={this.getPanel(this.getRawCustomDataForFiles())} />
+              <Tab2 id='all' title='All Metadata' panel={this.getPanel(this.getRawDataForFiles())} />
+            </Tabs2>
+            <TagsEditor
+              files={files}
+              submitHandler={this.submitCustomData}
+              allTags={props.allTags}
+            />
+            <CustomFieldsEditor
+              files={files}
+              allCustomFieldsKeys={props.allCustomFieldsKeys}
+              submitHandler={this.submitCustomData}
+            />
+            <div className='center'>
+              <Button
+                className='pt-intent-danger mt-30'
+                onClick={() => window.confirm('Are you sure?') && this.removeAllData()}
+              >remove all data</Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
